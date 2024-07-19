98ce0a8a9dae401a34fc5fe48d4787376086521f
Rupun Chathuranga <rupun.cj@gmail.com>
2024-07-19 22:13:15 +0530

    1. Pagination implemented

--- src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection.Metadata;
+using Marten.Pagination;
 
 namespace CatalogAPI.Products.GetProducts;
 
-public record GetProductQuery() : IQuery<GetProductResult>;
+public record GetProductQuery(int? PageNumber =2,int? PageSize =1) : IQuery<GetProductResult>;
 
 public record GetProductResult(IEnumerable<Product> Products);
 public class GetProductsHandler(IDocumentSession session) : IQueryHandler<GetProductQuery, GetProductResult>
 {
     public async Task<GetProductResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
     {
-        var Products = await session.Query<Product>().ToListAsync();
+        var Products = await session.Query<Product>().ToPagedListAsync(query.PageNumber?? 1, query.PageSize ?? 1);
         return new GetProductResult(Products);
     }
 }
