93690042a348136d6fa9699638c1afba58b93311
Rupun Chathuranga <rupun.cj@gmail.com>
2024-07-21 23:02:04 +0530

    1. implement decorator pattern to redis caching 2. config service container for decorator using scrutor package 3.config redis caching by using AddStackExchangeRedisCache

---
+++ src/Services/Basket/BasketApi/Data/CachedBasketRepository.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace BasketApi.Data;
+
+public class CachedBasketRepository (IBasketRepository repository,IDistributedCache cache): IBasketRepository
+{
+    public async Task<bool> DeleteBasket(string userName)
+    {
+       await cache.RemoveAsync(userName);
+       return await repository.DeleteBasket(userName);
+    }
+
+    public async Task<ShopingCart> GetBasket(string userName)
+    {
+        var cachedBasket = await cache.GetStringAsync(userName);
+        if (!string.IsNullOrWhiteSpace(cachedBasket))
+        {
+            return JsonSerializer.Deserialize<ShopingCart>(cachedBasket)!;
+        }
+        var basket = await repository.GetBasket(userName);
+        await cache.SetStringAsync(userName,JsonSerializer.Serialize(basket));
+        return basket;
+    }
+
+    public async Task<ShopingCart> StoreBasket(ShopingCart basket)
+    {
+        await repository.StoreBasket(basket);
+        await cache.SetStringAsync(basket.Username, JsonSerializer.Serialize(basket));
+        return basket;
+    }
+}
--- src/Services/Basket/BasketApi/Program.cs
+++ src/Services/Basket/BasketApi/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddMarten(options =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository,BasketRepository>();
+builder.Services.Decorate<IBasketRepository,CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis")!;
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
